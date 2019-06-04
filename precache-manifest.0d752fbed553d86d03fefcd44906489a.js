87742c0bde2dfcc80da2dca65e9d80ec0210bc60
Chomo Gwom <chomogwom@gmail.com>
2019-06-04 23:23:03 +0200

    Updates

---
+++ precache-manifest.0d752fbed553d86d03fefcd44906489a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e339009a7626e961e306d0f7cdec9bf",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "fac5ba80c1dcb75307c5",
+    "url": "/Random-Quote-Generator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ab090f76ae3573ce4e6a",
+    "url": "/Random-Quote-Generator/static/css/main.89d5aef1.chunk.css"
+  },
+  {
+    "revision": "fac5ba80c1dcb75307c5",
+    "url": "/Random-Quote-Generator/static/js/2.a2f811e3.chunk.js"
+  },
+  {
+    "revision": "ab090f76ae3573ce4e6a",
+    "url": "/Random-Quote-Generator/static/js/main.4c89d54d.chunk.js"
+  },
+  {
+    "revision": "6f23a534c95c8651034b",
+    "url": "/Random-Quote-Generator/static/js/runtime~main.618461f0.js"
+  }
+]);
